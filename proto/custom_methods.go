fa1cd95022ce05ddf7cfa000f506a711021f3a42
jstalex <a1s3rp.v@gmail.com>
2023-05-26 08:48:26 +0300

    add round in ToFloat methods

--- proto/custom_methods.go
+++ proto/custom_methods.go
@@ -8,7 +8,11 @@ import (
 // ToFloat - get value as float64 number
 func (q *Quotation) ToFloat() float64 {
 	if q != nil {
-		return float64(q.Units) + float64(q.Nano)*math.Pow10(-9)
+		num := float64(q.Units) + float64(q.Nano)*math.Pow10(-9)
+		num = num * math.Pow10(9)
+		num = math.Round(num)
+		num = num / math.Pow10(9)
+		return num
 	}
 	return float64(0)
 }
@@ -16,7 +20,11 @@ func (q *Quotation) ToFloat() float64 {
 // ToFloat - get value as float64 number
 func (mv *MoneyValue) ToFloat() float64 {
 	if mv != nil {
-		return float64(mv.Units) + float64(mv.Nano)*math.Pow10(-9)
+		num := float64(mv.Units) + float64(mv.Nano)*math.Pow10(-9)
+		num = num * math.Pow10(9)
+		num = math.Round(num)
+		num = num / math.Pow10(9)
+		return num
 	}
 	return float64(0)
 }
