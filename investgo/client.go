fcf5442987785b2d0bfe8f67f8df60bc697dfb71
jstalex <a1s3rp.v@gmail.com>
2023-05-31 13:09:39 +0300

    add default endpoint

--- investgo/client.go
+++ investgo/client.go
@@ -84,6 +84,9 @@ func setDefaultConfig(conf *Config) {
 	if strings.Compare(conf.AppName, "") == 0 {
 		conf.AppName = "invest-api-go-sdk"
 	}
+	if strings.Compare(conf.EndPoint, "") == 0 {
+		conf.EndPoint = "sandbox-invest-public-api.tinkoff.ru:443"
+	}
 	if conf.DisableAllRetry {
 		conf.MaxRetries = 0
 	} else if conf.MaxRetries == 0 {
--- investgo/config.go
+++ investgo/config.go
@@ -8,20 +8,21 @@ import (
 
 // Config - структура для кофигурации SDK
 type Config struct {
-	// EndPoint - Для работы с реальным контуром и контуром песочницы нужны разные эндпоинты
+	// EndPoint - Для работы с реальным контуром и контуром песочницы нужны разные эндпоинты.
+	// По умолчанию = sandbox-invest-public-api.tinkoff.ru:443
 	//https://tinkoff.github.io/investAPI/url_difference/
 	EndPoint string `yaml:"EndPoint"`
-	// Token - Ваш токен для апи
+	// Token - Ваш токен для Tinkoff InvestAPI
 	Token string `yaml:"APIToken"`
 	// AppName - Название вашего приложения, по умолчанию = tinkoff-api-go-sdk
 	AppName string `yaml:"AppName"`
 	// AccountId - Если уже есть аккаунт для апи можно указать напрямую,
-	//для песочницы создастся и зпишется автоматически
+	// для песочницы создастся и запишется автоматически
 	AccountId string `yaml:"AccountId"`
 	// DisableResourceExhaustedRetry - Если true, то сдк не пытается ретраить, после получения ошибки об исчерпывании
 	// лимита запросов, если false, то сдк ждет нужное время и пытается выполнить запрос снова. По умолчанию = false
 	DisableResourceExhaustedRetry bool `yaml:"DisableResourceExhaustedRetry"`
-	// DisableAllRetry - отключение всех ретраев
+	// DisableAllRetry - Отключение всех ретраев
 	DisableAllRetry bool `yaml:"DisableAllRetry"`
 	// MaxRetries - Максимальное количество попыток переподключения, по умолчанию = 3
 	// (если указать значение 0 это не отключит ретраи, для отключения нужно прописать DisableAllRetry = true)
